b1e16fd269330865ee23e37098456280f0c2935d
ChoiSunHo <43162521+Fuenkel@users.noreply.github.com>
2018-10-16 14:52:51 +0900

    Update blink.ino
    delay 5000->10000

--- blink.ino
+++ blink.ino
@@ -20,7 +20,7 @@ void loop() {
   digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(5000);               // wait for a second
   digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
-  delay(5000);               // wait for a second
+  delay(10000);               // wait for a second
 }
 
 void hello(){
